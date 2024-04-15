b8cc8e607c693d4e4ade96beaff42d9449693ae8
murilo orias <mu.a.o@outlook.com>
2024-04-15 17:49:19 -0300

    adição do javscript

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 const yourAtackPoints = document.getElementById('attackPoints');
 const initBtn = document.getElementById('initGame');
-const cabeçaimages = ["", "image2.jpg", "image3.jpg"]; // Substitua pelos nomes reais das suas imagens
+const cabeçaimages = ["cabeçaCapivara.", "image2.jpg", "image3.jpg"]; // Substitua pelos nomes reais das suas imagens
 
 function generateattackPoints(){
     return Math.floor(Math.random()* 500) + 1;
